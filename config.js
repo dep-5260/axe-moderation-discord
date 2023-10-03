4b57aa964dc4d9e8225b638c72914007aeefd199
dep-5260 <jo.mpsimbolon@gmail.com>
2023-10-03 15:53:50 +0200

    Commit

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    token: "OTU1NjIxMjQ5MDAwODI5MDIw.GpxPRb.MBwIY8P1K-iCFrVCEt7fEGJK2KzuCg1AanhiyM"
+    token: "OTU1NjIxMjQ5MDAwODI5MDIw.GKuaqg.qQjRKSPgkbyeo08YXklZsUPQEOvstTFSyMXpoA"
 }
